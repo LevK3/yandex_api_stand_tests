2b6f6887ce3d2ab01da9e03a1e99624f11bc6c94
Lev Konyshev <lion.konyshev@yandex.ru>
2024-04-23 16:01:35 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,9 @@
+URL_SERVICE = "https://d70a72b4-f865-4df5-ab11-85340f09bfbc.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+# CREATE_USER_PATH хранит путь к API-методу для создания нового пользователя.
+# Этот путь будет использоваться для формирования полного URL-адреса в сочетании с базовым URL сервиса,
+# когда необходимо выполнить запрос на создание пользователя.
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
